907ecfdad92e55862c9254ecfff00eef63f0f53d
Drew Marino <drewmarino25@gmail.com>
2025-04-24 14:52:07 -0400

    added support for octant blocks + renamed
    also added:
    - manpage
    - reading image from stdin
    - a call to setlocale
    - POSIXified Makefile
    
    and changed name

--- Makefile
+++ Makefile
@@ -1,22 +1,31 @@
-VERSION = 0.1
+.POSIX:
+.PHONY: clean install uninstall
+
+VERSION = 0.2
 
 PREFIX = /usr/local
 BINDIR = ${PREFIX}/bin
+MANDIR = ${PREFIX}/share/man/man1
 
 INC = -I. -I/usr/include
-LIB = -lImlib2
+LIB = `pkg-config imlib2 --libs || echo -lImlib2`
 DEF = -DVERSION=\"${VERSION}\"
 
 CFLAGS = -Wall -Wextra -std=c99 -pedantic ${INC} -Os ${DEF}
 LDFLAGS = ${LIB} -s
 
-all: img2sext
+all: img2tant
 
-img2sext: main.c
+img2tant: main.c
 	${CC} ${CFLAGS} ${LDFLAGS} main.c -o $@
 
-install: img2sext
-	cp img2sext ${BINDIR}/img2sext
+clean:
+	rm -f img2tant
+
+install: img2tant
+	cp img2tant ${BINDIR}/img2tant
+	sed img2tant.1 -e "s/VERSION/v${VERSION}/" > "${MANDIR}/img2tant.1"
 
 uninstall:
-	rm -f ${BINDIR}/img2sext
+	rm -f ${BINDIR}/img2tant
+	rm -f ${MANDIR}/img2tant.1
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-/* img2sext: renders images as sextant blocks */
+/* img2tant: renders images as either sextant or octant blocks
+ * this code is unlicensed and released to the public domain */
 #define _POSIX_C_SOURCE 200809L
 
 #include <stdarg.h>
@@ -6,25 +7,29 @@
 #include <stdlib.h>
 
 #include <errno.h>
+#include <fcntl.h>
+#include <locale.h>
 #include <string.h>
-
 #include <unistd.h>
 
 #include <Imlib2.h>
 
-#define pix(x,y) (pixels[((y) * width) + (x)])
-#define is_white(x) ((((x) & 0xFFFFFF) >= 0x7f7f7f) ^ inv)
+#include "tants.h"
 
-/* I am aware there are better ways to do this, but i don't really care. I did
- * this because there is no sextant 135 or 246, and I didn't want to add edge-
- * cases.*/
-const char* sextants[] = {" ", "🬀", "🬁", "🬂", "🬃", "🬄", "🬅", "🬆", "🬇", "🬈", "🬉",
-                          "🬊", "🬋", "🬌", "🬍", "🬎", "🬏", "🬐", "🬑", "🬒", "🬓", "▌",
-                          "🬔", "🬕", "🬖", "🬗", "🬘", "🬙", "🬚", "🬛", "🬜", "🬝", "🬞",
-                          "🬟", "🬠", "🬡", "🬢", "🬣", "🬤", "🬥", "🬦", "🬧", "▐", "🬨",
-                          "🬩", "🬪", "🬫", "🬬", "🬭", "🬮", "🬯", "🬰", "🬱", "🬲", "🬳",
-                          "🬴", "🬵", "🬶", "🬷", "🬸", "🬹", "🬺", "🬻", "█"};
-const char* shortopts = ":hio:v";
+#define ISOCT (tants == &octants)
+#define PIX(x,y) (pixels[((y) * width) + (x)])
+#define ISWHITE(x) ((((x) & 0xFFFFFF) >= 0x7f7f7f) ^ inv)
+
+const char* imlib_errs[] = {
+	[0] = "Success",
+	[-IMLIB_ERR_INTERNAL] = "Internal error (should not happen)",
+	[-IMLIB_ERR_NO_LOADER] = "No loader for file format",
+	[-IMLIB_ERR_NO_SAVER] = "No saver for file format",
+	[-IMLIB_ERR_BAD_IMAGE] = "Invalid image file",
+	[-IMLIB_ERR_BAD_FRAME] = "Requested frame not in image",
+};
+
+const char* shortopts = ":68hio:v";
 
 static void die(int erno, char* message, ...);
 static void usage(void);
@@ -33,17 +38,25 @@ static void version(void);
 int
 main(int argc, char** argv)
 {
-	Imlib_Image image;
-	Imlib_Load_Error error_return;
-	int x, y, opt, inv, sextant, width, height, moreWidth, moreHeight;
-	uint32_t *pixels;
+	const char *(*tants)[];
+	char *filename, buf[256];
 	FILE* output;
+	Imlib_Image image;
+	int x, y, fd, opt, len, inv, bits, width, height, moreWidth, moreHeight;
+	uint32_t *pixels;
 
 	inv = 0;
+	tants = &sextants;
 	output = stdout;
 
 	while ((opt = getopt(argc, argv, shortopts)) != -1) {
 		switch (opt) {
+		case '6':
+			tants = &sextants;
+			break;
+		case '8':
+			tants = &octants;
+			break;
 		case 'i':
 			inv = 1;
 			break;
@@ -51,7 +64,7 @@ main(int argc, char** argv)
 			output = fopen(optarg, "w");
 			
 			if (output == NULL)
-				die(5, "cannot open output file: %s", strerror(errno));
+				die(6, "cannot open output file: %s", strerror(errno));
 			break;
 		case '?':
 			usage();
@@ -73,12 +86,45 @@ main(int argc, char** argv)
 
 	if (optind == argc)
 		die(3, "no image filename given");
+	else if (optind > argc + 1)
+		die(4, "too many positional arguments");
 
-	image = imlib_load_image_with_error_return(argv[optind], &error_return);
+	if (strcmp(argv[optind], "-") == 0) {
+		filename = strdup("/tmp/img2tantXXXXXX");
+		fd = mkstemp(filename);
 
-	if (image == NULL)
-		die(4, "failed to open file: %s (imlib error %d)", strerror(errno),
-		    error_return);
+		if (fd == -1)
+			die(7, "failed to create temporary file: %s", strerror(errno));
+
+		while ((len = read(STDIN_FILENO, buf, 256)) == 256)
+			write(fd, buf, len);
+		if (len > 0)
+			write(fd, buf, len);
+
+		close(fd);
+	} else {
+		filename = argv[optind];
+		fd = 0;
+	}
+
+	image = imlib_load_image_with_errno_return(filename, &errno);
+
+	if (fd) {
+		unlink(filename);
+		free(filename);
+	}
+
+	if (image == NULL) {
+		if (errno > 0)
+			die(5, "failed to open image: %s", strerror(errno));
+		else if (errno < 0) {
+			if (errno < IMLIB_ERR_BAD_FRAME)
+				die(5, "failed to open image: imlib error #%i", -errno);
+			else
+				die(5, "failed to open image: imlib error: %s",
+				    imlib_errs[-errno]);
+		}
+	}
 
 	imlib_context_set_image(image);
 
@@ -86,41 +132,53 @@ main(int argc, char** argv)
 	moreWidth = width % 2;
 
 	height = imlib_image_get_height();
-	moreHeight = height % 3;
+	moreHeight = height % (ISOCT ? 4 : 3);
 
 	pixels = imlib_image_get_data_for_reading_only();
 
-	for (y = 0; y < (height - moreHeight); y += 3) {
+	setlocale(LC_ALL, "");
+
+	for (y = 0; y < (height - moreHeight); y += (ISOCT ? 4 : 3)) {
 		for (x = 0; x < (width - moreWidth); x += 2) {
-			sextant = 0;
+			bits = 0;
 
-			if (is_white(pix(x,y)))
-				sextant |= 0x1;
-			if (is_white(pix(x+1,y)))
-				sextant |= 0x2;
-			if (is_white(pix(x,y+1)))
-				sextant |= 0x4;
-			if (is_white(pix(x+1,y+1)))
-				sextant |= 0x8;
-			if (is_white(pix(x,y+2)))
-				sextant |= 0x10;
-			if (is_white(pix(x+1,y+2)))
-				sextant |= 0x20;
+			if (ISWHITE(PIX(x,y)))
+				bits |= 0x1;
+			if (ISWHITE(PIX(x+1,y)))
+				bits |= 0x2;
+			if (ISWHITE(PIX(x,y+1)))
+				bits |= 0x4;
+			if (ISWHITE(PIX(x+1,y+1)))
+				bits |= 0x8;
+			if (ISWHITE(PIX(x,y+2)))
+				bits |= 0x10;
+			if (ISWHITE(PIX(x+1,y+2)))
+				bits |= 0x20;
+			if (ISOCT) {
+				if (ISWHITE(PIX(x,y+3)))
+					bits |= 0x40;
+				if (ISWHITE(PIX(x+1,y+3)))
+					bits |= 0x80;
+			}
 
-			fprintf(output, sextants[sextant]);
+			fprintf(output, (*tants)[bits]);
 		}
 
 		if (moreWidth > 0) {
-			sextant = 0;
+			bits = 0;
 
-			if (is_white(pix(x,y)))
-				sextant |= 0x1;
-			if (is_white(pix(x,y+1)))
-				sextant |= 0x4;
-			if (is_white(pix(x,y+2)))
-				sextant |= 0x10;
+			if (ISWHITE(PIX(x,y)))
+				bits |= 0x1;
+			if (ISWHITE(PIX(x,y+1)))
+				bits |= 0x4;
+			if (ISWHITE(PIX(x,y+2)))
+				bits |= 0x10;
+			if (ISOCT) {
+				if (ISWHITE(PIX(x,y+3)))
+					bits |= 0x40;
+			}
 
-			fprintf(output, sextants[sextant]);
+			fprintf(output, (*tants)[bits]);
 		}
 
 		fprintf(output, "\n");
@@ -128,36 +186,49 @@ main(int argc, char** argv)
 
 	if (moreHeight > 0) {
 		for (x = 0; x < (width - moreWidth); x += 2) {
-			sextant = 0;
+			bits = 0;
 
-			if (is_white(pix(x,y)))
-				sextant |= 0x1;
-			if (is_white(pix(x+1,y)))
-				sextant |= 0x2;
+			if (ISWHITE(PIX(x,y)))
+				bits |= 0x1;
+			if (ISWHITE(PIX(x+1,y)))
+				bits |= 0x2;
 			if (moreHeight > 1) {
-				if (is_white(pix(x,y+1)))
-					sextant |= 0x4;
-				if (is_white(pix(x+1,y+1)))
-					sextant |= 0x8;
+				if (ISWHITE(PIX(x,y+1)))
+					bits |= 0x4;
+				if (ISWHITE(PIX(x+1,y+1)))
+					bits |= 0x8;
+				if (moreHeight > 2) {
+					if (ISWHITE(PIX(x,y+2)))
+						bits |= 0x10;
+					if (ISWHITE(PIX(x+1,y+2)))
+						bits |= 0x20;
+				}
 			}
 
-			fprintf(output, sextants[sextant]);
+			fprintf(output, (*tants)[bits]);
 		}
 
 		if (moreWidth > 0) {
-			sextant = 0;
+			bits = 0;
 
-			if (is_white(pix(x,y)))
-				sextant |= 0x1;
-			if (moreHeight > 1)
-				if (is_white(pix(x,y+1)))
-					sextant |= 0x4;
+			if (ISWHITE(PIX(x,y)))
+				bits |= 0x1;
+			if (moreHeight > 1) {
+				if (ISWHITE(PIX(x,y+1)))
+					bits |= 0x4;
+				if (moreHeight > 2) {
+					if (ISWHITE(PIX(x,y+2)))
+						bits |= 0x10;
+					if (ISWHITE(PIX(x+1,y+2)))
+						bits |= 0x20;
+				}
+			}
 
-			fprintf(output, sextants[sextant]);
+			fprintf(output, (*tants)[bits]);
 		}
 	}
 
-	imlib_free_image();
+	imlib_free_image_and_decache();
 
 	return 0;
 }
@@ -168,7 +239,7 @@ die(int erno, char* message, ...)
 	va_list ap;
 
 	va_start(ap, message);
-	fprintf(stderr, "img2sext: ");
+	fprintf(stderr, "img2tant: ");
 	vfprintf(stderr, message, ap);
 	fprintf(stderr, "\n");
 	va_end(ap);
@@ -179,27 +250,19 @@ die(int erno, char* message, ...)
 static void
 usage(void)
 {
-	puts("Usage: img2sext [-hiv] [-o FILE] IMAGEFILE\n"
+	puts("Usage: img2tant [-hiv] [-o FILE] IMAGEFILE\n"
 	     "\n"
+	     "  -6    force the use of sextant blocks\n"
+	     "  -8    force the use of octant blocks\n"
 	     "  -i    treat white pixels as black and black pixels as white\n"
 	     "  -o    send output to file instead of stdout\n"
 	     "  -h    print help and exit\n"
-	     "  -v    print version and exit\n"
-	     "\n"
-	     "Exit Codes:\n"
-	     "\n"
-	     "  1    unknown argument\n"
-	     "  2    no output file path given\n"
-	     "  3    no image filename given\n"
-	     "  4    failed to open file\n"
-	     "  5    cannot open output file\n"
-	     "\n"
-	     "Behavior is undefined for pixels that are not either white or black");
+	     "  -v    print version and exit");
 }
 
 static void
 version(void)
 {
-	puts("img2sext v"VERSION"\n"
+	puts("img2tant v"VERSION"\n"
 	     "written by Drew Marino (OneTrueC)");
 }
